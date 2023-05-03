dfc9a3c7ab8f4cd5f19fc79de58eca37bd0c51d3
Hampus Isebring <hampus.isebring@gmail.com>
2023-05-03 22:52:15 +0200

    added more content to landing page

--- client/src/pages/Landingpage.tsx
+++ client/src/pages/Landingpage.tsx
@@ -1,9 +1,22 @@
-import { Box, Flex, Text, Title } from '@mantine/core';
+import {
+  Avatar,
+  Box,
+  Button,
+  Flex,
+  Group,
+  Input,
+  MediaQuery,
+  Stack,
+  Text,
+  Title,
+  Tooltip
+} from '@mantine/core';
+import { IconCircleCheck, IconUser } from '@tabler/icons-react';
 
 function LandingPage() {
   return (
     <Flex align="center" justify="space-between">
-      <Box sx={{ flex: 1 }}>
+      <Box  sx={{ flex: 1, marginLeft: 'auto' }}>
         <Title pl="lg">
           AvianBanter is a Chat Application built for everyone with a beak
         </Title>
@@ -11,14 +24,42 @@ function LandingPage() {
           Here you can create or join chat rooms. You can also send private
           messages.
         </Text>
+        <Stack mt="md" align="center" spacing="xs">
+          <Avatar size="lg" radius="xl"></Avatar>
+          <Text>Choose an avatar</Text>
+          <Input
+            mt="md"
+            icon={<IconUser size="1rem" />}
+            placeholder="Your username..."
+            rightSection={
+              <Tooltip label="Username available" position="top-end" withArrow>
+                <div>
+                  <IconCircleCheck
+                    size="1rem"
+                    style={{ display: 'block', opacity: 0.5 }}
+                  />
+                </div>
+              </Tooltip>
+            }
+          />
+        </Stack>
+        <Group spacing="sm" position='center' mt="xl">
+          <Button variant="filled">Create a room</Button>
+          <Button variant="outline">Join a room</Button>
+        </Group>
       </Box>
+       <MediaQuery
+      query="(max-width: 700px)"
+      styles={{ display: 'none' }}
+    >
       <Box sx={{ flex: 1, marginLeft: 'auto' }}>
         <img
-          src="/avian-landingpage.png"
-          alt="Birds chatting with each other"
-          style={{ width: '100%', height: '100%', objectFit: 'cover' }}
+          src="/avian-phone.png"
+          alt="Bird browsing a smartphone"
+          style={{ width: '100%', height: '100vh', objectFit: 'cover' }}
         />
       </Box>
+      </MediaQuery> 
     </Flex>
   );
 }
