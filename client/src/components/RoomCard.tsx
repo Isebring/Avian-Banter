95a3d506f9c5c66b6584a2ee8160e7c819799d6e
Gabriel Lugo Méndez <gabriel.lugo.mendez@gmail.com>
2023-05-05 14:10:21 +0200

    update card-styling

--- client/src/components/RoomCard.tsx
+++ client/src/components/RoomCard.tsx
@@ -2,9 +2,21 @@ import { Button, Card, Text, Title } from '@mantine/core';
 
 function RoomCard() {
   return (
-    <Card shadow="sm" radius="md" padding="lg" withBorder>
-      <Title>RoomName</Title>
-      <Text>Users</Text>
+    <Card
+      shadow="sm"
+      radius="md"
+      padding="lg"
+      m="md"
+      withBorder
+      sx={{
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        justifyContent: 'spaceBetween',
+      }}
+    >
+      <Title align="center">RoomName</Title>
+      <Text m="md">Users</Text>
       <Button>Join</Button>
     </Card>
   );
