e4650012374fb90f9c37c7f73ed572a7fcbe4748
Emil Helgesson <helgesson.emil.94@gmail.com>
2023-05-09 13:28:29 +0200

    import SocketData

--- client/src/context/UsernameContext.tsx
+++ client/src/context/UsernameContext.tsx
@@ -1,4 +1,5 @@
 import { createContext, useContext, useEffect, useState } from 'react';
+import { SocketData } from '../../../server/communication';
 import { socket } from './SocketContext';
 
 interface UsernameContextType {
@@ -37,7 +38,7 @@ export const UsernameProvider: React.FC<UsernameProviderProps> = ({
   }, []);
 
   useEffect(() => {
-    function handleSession({ sessionID, userID }: SocketData) {
+    function handleSession({ sessionID }: SocketData) {
       // attach the session ID to the next reconnection attempts
       socket.auth = { sessionID };
       // store it in the localStorage
